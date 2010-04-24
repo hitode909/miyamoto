7a45ba96abd156e4705eb2a979f223a403a5fd94
udonchan <udonchan@udonchan.org>
2010-04-24 14:15:10 +0900

    add submodule

--- main.rb
+++ main.rb
@@ -1,8 +1,21 @@
 require 'rubygems'
 require 'uri'
+require 'ruby-mplayer/lib/mplayer'
 
-library_path = File.expand_path('~/Music/iTunes/iTunes Music Library.xml')
-home_path = File.expand_path('~')
-library = open(library_path).read
+module Foo 
 
-puts library.scan(%r|>file:\/\/localhost(.*)<|).flatten.map{|s| URI.unescape(s)}
+  @library = open(File.expand_path('~/Music/iTunes/iTunes Music Library.xml')).read
+
+  def self.get_next_track
+    loop do
+      file = URI.unescape(@library.scan(%r|>file:\/\/localhost(.*)<|).flatten.choice)
+      return file if File.exist?(file)
+    end
+  end
+
+end
+
+mplayer = Mplayer.new
+p mplayer
+mplayer.play(Foo.get_next_track)
+
