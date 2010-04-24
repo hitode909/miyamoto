729a5854dac353552810ca0a923dd857b9ed72a1
hitode909 <hitode909@gmail.com>
2010-04-24 16:08:45 +0900

    mplayer.rb is class definition.

--- mplayer.rb
+++ mplayer.rb
@@ -99,26 +99,3 @@ class MPlayer
     @io.__send__(func, *args)
   end
 end
-
-
-mplayer = MPlayer.new
-
-
-loop {
-  track = Foo.get_next_track
-  next unless File.file?(track)
-
-  puts "playing #{track}"
-  mplayer.play(track.gsub(' ', '\ '))
-  mplayer.seek_to_percent(80)
-
-  speed = 2
-  while mplayer.playing?
-    mplayer.pause
-    system 'say good morning!'
-    mplayer.pause
-    speed += 0.1
-    mplayer.set_speed(speed)
-    sleep 5
-  end
-}
