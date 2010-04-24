ae1c2e2e578920d5e663d5039809182052e930b2
udonchan <udonchan@udonchan.org>
2010-04-24 16:45:43 +0900

    impl timer

--- main.rb
+++ main.rb
@@ -2,22 +2,26 @@ require 'rubygems'
 require 'mplayer'
 
 mplayer = MPlayer.new
+timer = Time.now + (ARGV.first || 0.1).to_f * 60
+is_past = false
 
-loop {
+loop do
   track = Foo.get_next_track
   next unless File.file?(track)
 
   puts "playing #{track}"
   mplayer.play(track.gsub(' ', '\ '))
   mplayer.seek_to_percent(80)
+  speed = 1.0
 
-  speed = 2
   while mplayer.playing?
-    mplayer.pause
-    system 'say good morning!'
-    mplayer.pause
-    speed += 0.1
-    mplayer.set_speed(speed)
-    sleep 5
+    if Time.now > timer && !is_past
+      mplayer.pause
+      system "mplayer #{File.dirname(__FILE__)}/se.wav >& /dev/null"
+      speed += 0.5
+      mplayer.pause
+      mplayer.set_speed(speed)
+      is_past = true
+    end
   end
-}
+end
