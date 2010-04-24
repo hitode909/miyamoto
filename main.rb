f41d4c6537a95f8febda84dc430d40dc96311625
udonchan <udonchan@udonchan.org>
2010-04-24 13:29:11 +0900

    add uri unescape

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
-require 'nokogiri'
+require 'uri'
 
 library_path = File.expand_path('~/Music/iTunes/iTunes Music Library.xml')
 home_path = File.expand_path('~')
 library = open(library_path).read
 
-puts library.scan(%r|>file:\/\/localhost(.*)<|).flatten
+puts library.scan(%r|>file:\/\/localhost(.*)<|).flatten.map{|s| URI.unescape(s)}
